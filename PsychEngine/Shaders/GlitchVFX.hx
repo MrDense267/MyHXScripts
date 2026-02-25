7910342c6757103fc8efbd2776a5140bab121b12
Dense <dense267@gmail.com>
2026-02-25 03:46:57 -0500

    Create GlitchVFX shader class
    Added a custom shader class for glitch effects.

---
+++ PsychEngine/Shaders/GlitchVFX.hx
@@ -0,0 +1,12 @@
+// GlitchVFX.hx - Custom shader for Psych Engine V0.6.3 source code specifically.
+// You may need to change some code for more newer versions.
+// Script written by EchoingOverfreeze.
+
+package funkin;
+
+import flixel.FlxShader;
+
+class GlitchVFX extends FlxShader
+{
+
+}
